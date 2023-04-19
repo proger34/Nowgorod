2b23ce93f34f4b72b9a94d0107cf5bcb502660d4
Proger34 <Programist1235@gmail.com>
2023-04-19 14:36:04 +0300

    lol

---
+++ Assets/Scripts/PistolFire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PistolFire : MonoBehaviour
+{
+    public AudioSource audioSource; 
+    public Animator anim;
+    public AudioClip fireClip;
+
+    Interactable interactable;
+    public SteamVR_Action_Boolean fireAction;
+    public Transform barel;
+
+    private void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void Update()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            SteamVR_Input_Sources hand = interactable.attachedToHand.handType;
+
+            if (fireAction[hand].stateDown)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        audioSource.PlayOneShot(fireClip);
+        anim.Play("Fire");
+
+        if (Physics.Raycast(barel.position, barel.forward, out hit, 300))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
